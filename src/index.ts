169b815ee97dfc4f13a947be23565e96d4c3b42f
Swaraj Gandhi <swaraj@browserstack.com>
2022-04-06 09:41:01 +0530

    added changes for the https server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import fs from 'fs';
+import https from 'https';
 import path from 'path';
 import routes from './routes';
 
 const app = express();
 const port = 3000;
+const privateKey = fs.readFileSync('key.pem');
+const certificate = fs.readFileSync('cert.pem');
+const credentials = { key: privateKey, cert: certificate };
 
 // static files
 app.use(express.static('public'));
@@ -14,6 +19,9 @@ app.set('views', path.join(__dirname, '/views'));
 
 app.use(routes);
 
-app.listen(port, () => {
-  console.log(`Timezones by location application is running on port ${port}.`);
-});
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(port);
+
+// app.listen(port, () => {
+//   console.log(`Timezones by location application is running on port ${port}.`);
+// });
