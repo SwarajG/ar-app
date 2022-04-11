285d3ebb9dad8664444315e3804fe3c980cc5a27
Swaraj Gandhi <swaraj@browserstack.com>
2022-04-11 20:39:15 +0530

    test

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import fs from 'fs';
 import http from 'http';
-// import https from 'https';
+import https from 'https';
 import path from 'path';
 import routes from './routes';
 
 const app = express();
-// const HTTPS_PORT = process.env.PORT || 3000;
+const HTTPS_PORT = process.env.PORT || 3000;
 const HTTP_PORT = process.env.PORT || 8080;
 const privateKey = fs.readFileSync('key.pem');
 const certificate = fs.readFileSync('cert.pem');
@@ -23,13 +23,15 @@ app.set('view engine', 'ejs');
 
 app.use(routes);
 
-const httpServer = http.createServer(app);
-// const httpsServer = https.createServer(credentials, app);
+console.log(process.env.NODE_ENV);
 
-// https.createServer(credentials, app).listen(HTTPS_PORT, () => {
-//   console.log(`HTTPS Application is running on port ${HTTPS_PORT}.`);
-// });
-
-httpServer.listen(HTTP_PORT, () => {
-  console.log(`HTTP Application is running on port ${HTTP_PORT}.`);
-});
+if (process.env.NODE_ENV === 'production') {
+  const httpServer = http.createServer(app);
+  httpServer.listen(HTTP_PORT, () => {
+    console.log(`HTTP Application is running on port ${HTTP_PORT}.`);
+  });
+} else {
+  https.createServer(credentials, app).listen(HTTPS_PORT, () => {
+    console.log(`HTTPS Application is running on port ${HTTPS_PORT}.`);
+  });
+}
