5e4d83288565a55c8d597e41309a97b0a8e54d45
ashishsfb <ashishsfb@gmail.com>
2022-04-12 05:25:36 +0530

    Reticle group and all state parent aligns properly now

--- public/product/align-to-camera.js
+++ public/product/align-to-camera.js
@@ -1,21 +1,19 @@
 AFRAME.registerComponent('align-to-camera', {
     init(){
-        // this.reticleAligner = document.getElementById("reticle-aligner");
+        this.reticleAligner = document.getElementById("reticle-aligner");
         this.hotspotAligner = document.getElementById("hotspot-aligner");
     },
     tick(){
         let rot_x = this.el.getAttribute('rotation').x;
 
-        // let rot_y = this.reticleAligner.getAttribute('rotation').y;
+        let rot_y;
 
-        // if(this.el.getAttribute('id') === 'reticle'){
-        //     rot_y = this.reticleAligner.getAttribute('rotation').y;
-        // }
-        // else if(this.el.getAttribute('id') === 'all-states'){
-        //     rot_y = this.hotspotAligner.getAttribute('rotation').y;
-        // }
-        
-        let rot_y = this.hotspotAligner.getAttribute('rotation').y;
+        if(this.el.getAttribute('id') === 'reticle-group'){
+            rot_y = this.reticleAligner.getAttribute('rotation').y;
+        }
+        else if(this.el.getAttribute('id') === 'all-states'){
+            rot_y = this.hotspotAligner.getAttribute('rotation').y;
+        }
 
         let rot_z = this.el.getAttribute('rotation').z;
 
