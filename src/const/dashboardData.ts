857e7503501726a42d8ed357aa146a2a077563a0
ashishsfb <ashishsfb@gmail.com>
2022-05-02 02:06:56 +0530

    Folder Refactor

--- src/const/dashboardData.ts
+++ src/const/dashboardData.ts
@@ -3,24 +3,24 @@ export const dashboardData = [
     id: 'radio-review',
     name: 'reviewLabelText',
     value: '0',
-    image: '../images/dashboard/reviews-icon.png',
+    image: '../common/images/dashboard/reviews-icon.png',
   },
   {
     id: 'radio-our-science',
     name: 'ourScienceLabelText',
     value: '1',
-    image: '../images/dashboard/our-science-icon.png',
+    image: '../common/images/dashboard/our-science-icon.png',
   },
   {
     id: 'radio-texture',
     name: 'textureLabelText',
     value: '2',
-    image: '../images/dashboard/texture-icon.png',
+    image: '../common/images/dashboard/texture-icon.png',
   },
   {
     id: 'radio-frequently-bt',
     name: 'routinesLabelText',
     value: '3',
-    image: '../images/dashboard/frequently-bt-icon.png',
+    image: '../common/images/dashboard/frequently-bt-icon.png',
   },
 ];
