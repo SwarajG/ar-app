e89c893a17f2453e9d3cb4f38bcc5bbe5983d65b
bhaumikdebanshu <bhaumikdebanshu@gmail.com>
2022-05-02 18:34:31 +0530

    languages added and all html data access through language.ts

--- src/routes/language.ts
+++ src/routes/language.ts
@@ -1,42 +1,99 @@
 interface languageText {
   [key: string]: {
+    //Buttons
     closeButtonText: string,
+    resetButtonText: string,
   
-    // Dashboard Labels
+    //Dashboard Labels
     reviewLabelText: string,
     ourScienceLabelText: string,
     textureLabelText: string,
     routinesLabelText: string,
     
-    // Instructions
+    //Instructions
     swipeInstText: string,
     pinchScaleInstText: string,
+
+    //Modal Heads
+    ingredientsHead : string,
+    ceramidesHead : string,
+    efficacyHead : string,
+    howToApplyHead: string,
+
+    //Hotspot Labels
+    ingredientsHotspotLabel : string,
+    ceramidesHotspotLabel : string,
+    efficacyHotspotLabel : string,
+    lookInsideHotspotLabel : string,
+    howToApplyHotspotLabel : string,
+
+    //Place here message
+    placeHereText: string,
+
   }
 }
 
 export const languageText: languageText = {
   en: {
+    //Buttons
     closeButtonText: 'Close',
+    resetButtonText: 'Reset',
 
-    // Dashboard Labels En
+    //Dashboard Labels
     reviewLabelText: 'Reviews',
     ourScienceLabelText: 'Our Science',
     textureLabelText: 'Texture',
     routinesLabelText: 'Routines',
 
+    //instructions
     swipeInstText: 'Swipe to Rotate',
     pinchScaleInstText: 'Pinch to Scale',
+
+    //Modal Heads
+    ingredientsHead : 'Ingredients',
+    ceramidesHead : 'Science of Ceramides',
+    efficacyHead : 'Efficacy',
+    howToApplyHead: 'How to Apply',
+
+    //Hotspot Labels
+    ingredientsHotspotLabel : 'Ingredients',
+    ceramidesHotspotLabel : 'Ceramides',
+    efficacyHotspotLabel : 'Efficacy',
+    lookInsideHotspotLabel : 'Look Inside',
+    howToApplyHotspotLabel : 'How to Apply',
+
+    //Place here message
+    placeHereText: 'Place Here',
   },
   es: {
+    //Buttons
     closeButtonText: 'Cerrar',
+    resetButtonText: 'Reinicar',
     
-    // Dashboard Labels Es
+    //Dashboard Labels 
     reviewLabelText: 'Reseñas',
     ourScienceLabelText: 'Nuestra Ciencia',
     textureLabelText: 'Textura',
     routinesLabelText: 'Rutinas',
 
+    //Instructions
     swipeInstText: 'Deslizar para Rotar',
     pinchScaleInstText: 'Pellizcar a Escala',
+
+    //Modal Heads
+    ingredientsHead : 'Ingredientes',
+    ceramidesHead : 'Ciencia de las Ceramidas',
+    efficacyHead : 'Eficacia',
+    howToApplyHead: 'Cómo Aplicar',
+
+    //Hotspot Labels
+    ingredientsHotspotLabel : 'Ingredientes',
+    ceramidesHotspotLabel : 'Ceramidas',
+    efficacyHotspotLabel : 'Efficacia',
+    lookInsideHotspotLabel : 'Mirar Dentro',
+    howToApplyHotspotLabel : 'Como Aplicar',
+
+    //Place here message
+    placeHereText: 'Lugar Aqui',
   },
 };
